3212b25922e27b83c302d6b0d70ef1b13a2b8df9
CooperMoore9 <thefryman9@gmail.com>
2022-07-05 14:11:44 -0500

    Basic etch-a-scetch
    When you hover over a element they change to be filled out

--- javascript.js
+++ javascript.js
@@ -1,10 +1,16 @@
-let input = 4;
+let input = 20;
 const container = document.getElementById('gridContainer');
-container.style.gridTemplateColumns = `repeat(${input}, 1fr)`
+container.style.gridTemplateColumns = `repeat(${input}, 1fr)`;
 
 for(let i = 0; i < input * input; i++){
-    const gridBox = document.createElement('div')
-    gridBox.classList.add(`box${i}`)
-    gridBox.style.borderStyle = "dotted";
+    const gridBox = document.createElement('div');
+    gridBox.classList.add(`box`);
     container.appendChild(gridBox);
 }
+
+const hoverOverBox = document.querySelectorAll('.box');
+hoverOverBox.forEach((div) => {
+    div.addEventListener('mouseover', () => {
+        div.classList.add("filled")
+    })
+})
