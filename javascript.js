e980b7aedadde7c998424df562cc8ae9e402cbfa
CooperMoore9 <thefryman9@gmail.com>
2022-07-04 23:47:17 -0500

    GRID
    I have done it! GRID!

--- javascript.js
+++ javascript.js
@@ -1,12 +1,10 @@
 let input = 4;
 const container = document.getElementById('gridContainer');
-
+container.style.gridTemplateColumns = `repeat(${input}, 1fr)`
 
 for(let i = 0; i < input * input; i++){
     const gridBox = document.createElement('div')
     gridBox.classList.add(`box${i}`)
-    gridBox.style.minHeight = "50px";
-    gridBox.style.minWidth = "50px";
     gridBox.style.borderStyle = "dotted";
     container.appendChild(gridBox);
 }
