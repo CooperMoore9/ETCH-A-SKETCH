f4b4b1a39ca63d94bfa8547a8e38c1b434f27daa
CooperMoore9 <thefryman9@gmail.com>
2022-07-06 16:41:27 -0500

    Works as intended but.
    super laggy when sliding the bar up. I need to get the input from the slider then update

--- javascript.js
+++ javascript.js
@@ -1,7 +1,31 @@
 const container = document.getElementById('gridContainer');
 const slider = document.getElementById('slider');
 
-let input = 4;
+
+
+// let input = 4;
+// container.style.gridTemplateColumns = `repeat(${input}, 1fr)`;
+
+// for(let i = 0; i < input * input; i++){
+//     const gridBox = document.createElement('div');
+//     gridBox.classList.add(`box`);
+//     container.appendChild(gridBox);
+// }
+
+// const hoverOverBox = document.querySelectorAll('.box');
+// hoverOverBox.forEach((div) => {
+//     div.addEventListener('mouseover', () => {
+//         div.classList.add("filled");
+//     })
+// })
+
+slider.oninput = function(){
+    const deleteTheBoxes = document.querySelectorAll('.box');
+    deleteTheBoxes.forEach((div) => {
+        div.remove();
+    })
+ 
+    let input = slider.value;
 container.style.gridTemplateColumns = `repeat(${input}, 1fr)`;
 
 for(let i = 0; i < input * input; i++){
@@ -17,9 +41,5 @@ hoverOverBox.forEach((div) => {
     })
 })
 
-slider.oninput = function(){
-    hoverOverBox.forEach((div) => {
-        div.remove();
-    })
-    
+
 }
