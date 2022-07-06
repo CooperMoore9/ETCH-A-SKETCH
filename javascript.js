7f9a6d946ea521ad671102179b84203232a88718
CooperMoore9 <thefryman9@gmail.com>
2022-07-06 17:58:07 -0500

    Clear button added

--- javascript.js
+++ javascript.js
@@ -1,28 +1,18 @@
 const container = document.getElementById('gridContainer');
 const slider = document.getElementById('slider');
 
-
-
-// let input = 4;
-// container.style.gridTemplateColumns = `repeat(${input}, 1fr)`;
-
-// for(let i = 0; i < input * input; i++){
-//     const gridBox = document.createElement('div');
-//     gridBox.classList.add(`box`);
-//     container.appendChild(gridBox);
-// }
-
-// const hoverOverBox = document.querySelectorAll('.box');
-// hoverOverBox.forEach((div) => {
-//     div.addEventListener('mouseover', () => {
-//         div.classList.add("filled");
-//     })
-// })
+input = slider.value
+sliderChange(input);
 
 slider.onchange = function(){
     let input = slider.value;
-    
     sliderChange(input);
+}
+
+// For updating the div thats gonna show what input value is in real time
+slider.oninput = function(input){
+    input = slider.value;
+    document.getElementById('userRange').textContent = `${input} x ${input}`;
 }
 
 function sliderChange(input) {
@@ -47,3 +37,12 @@ hoverOverBox.forEach((div) => {
 })
 
 }
+
+const clearButton = document.getElementById('clear');
+    clearButton.addEventListener('click', () =>{
+        const clearBoxes = document.querySelectorAll('.box');
+        clearBoxes.forEach((div) => {
+            div.classList.remove('filled');
+        })
+    })
+
