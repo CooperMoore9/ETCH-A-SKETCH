f4592bf9db8c3f470d14d9a176497d984c3e3f5e
CooperMoore9 <thefryman9@gmail.com>
2022-07-07 15:56:57 -0500

    Little cleanup

--- javascript.js
+++ javascript.js
@@ -22,7 +22,6 @@ slider.onchange = function(){
     }
 }
 
-// For updating the div thats gonna show what input value is in real time
 slider.oninput = function(input){
     input = slider.value;
     document.getElementById('userRange').textContent = `${input} x ${input}`;
@@ -40,18 +39,16 @@ const clearButton = document.getElementById('clear');
 
 function sliderChange(input) {
     container.style.gridTemplateColumns = `repeat(${input}, 1fr)`;
-
     const deleteTheBoxes = document.querySelectorAll('.box');
     deleteTheBoxes.forEach((div) => {
         div.remove();
     })
 
-for(let i = 0; i < input * input; i++){
-    const gridBox = document.createElement('div');
-    gridBox.classList.add(`box`);
-    container.appendChild(gridBox);
-}
-
+    for(let i = 0; i < input * input; i++){
+        const gridBox = document.createElement('div');
+        gridBox.classList.add(`box`);
+        container.appendChild(gridBox);
+    }
 }
 
 drawButton.addEventListener('click', () =>{
